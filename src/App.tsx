b8df8b1237479a88353287bc41f84ec8b24df090
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-02-09 16:12:06 +0100

    remove duplicate top styling

--- src/App.tsx
+++ src/App.tsx
@@ -482,7 +482,6 @@ function BoardCardElement({
         <div
           style={{
             position: "absolute",
-            top: 20,
             left: "50%",
             top: "50%",
             transform: "translate(-50%, -50%)",
