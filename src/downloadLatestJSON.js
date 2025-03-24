7b19818cf3e4202e29261dc1ab59aef6ac7082d7
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-03-25 00:31:54 +0100

    change download to bun compatible, and download newest files

--- src/downloadLatestJSON.js
+++ src/downloadLatestJSON.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
+import { writeFileSync } from "fs";
 
 fetch("https://cdn.playthebazaar.com/bazaardesigndataprod/cards.json")
   .then((res) => res.text())
   .then((text) => {
-    fs.writeFileSync(__dirname + "/json/cards.json", text, "utf-8");
+    writeFileSync(__dirname + "/json/cards.json", text, "utf-8");
   });
 
 fetch("https://www.howbazaar.gg/api/monsterEncounterDays")
   .then((res) => res.text())
   .then((text) => {
-    fs.writeFileSync(__dirname + "/json/monsterEncounterDays.json", text);
+    writeFileSync(__dirname + "/json/monsterEncounterDays.json", text);
   });
 
 fetch("https://www.howbazaar.gg/api/items")
   .then((res) => res.text())
   .then((text) => {
-    fs.writeFileSync(__dirname + "/json/items.json", text);
+    writeFileSync(__dirname + "/json/items.json", text);
 
-    fs.writeFileSync(
+    writeFileSync(
       __dirname + "/json/ValidItemNames.json",
       JSON.stringify(
         JSON.parse(text).data.map((card) => card.name),
@@ -30,9 +30,9 @@ fetch("https://www.howbazaar.gg/api/items")
 fetch("https://www.howbazaar.gg/api/skills")
   .then((res) => res.text())
   .then((text) => {
-    fs.writeFileSync(__dirname + "/json/skills.json", text);
+    writeFileSync(__dirname + "/json/skills.json", text);
 
-    fs.writeFileSync(
+    writeFileSync(
       __dirname + "/json/ValidSkillNames.json",
       JSON.stringify(
         JSON.parse(text).data.map((card) => card.name),
