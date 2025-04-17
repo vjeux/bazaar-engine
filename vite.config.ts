d6bbc296ae692c7ce3e11c94e0ce9d0cbcfaa0ad
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-04-17 22:08:29 +0200

    move to deno config and add tanstack router dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "npm:@tanstack/router-plugin/vite";
+import deno from "@deno/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    deno(),
+    react()
+  ],
   server: {
     port: 3000
   },
