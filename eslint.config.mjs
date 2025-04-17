fe021280a8caf750822774ec6174da286efef029
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-04-17 22:31:03 +0200

    delete eslint in favour of deno lint

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import eslint from "npm:@eslint/js";
-import tseslint from "npm:typescript-eslint";
-
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
