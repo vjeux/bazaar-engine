db9659ec55258b10bda0b2454d3f5b3a91c19aed
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-04-19 20:33:10 +0200

    remove unused tooltip from frame page

--- app/frame/page.tsx
+++ app/frame/page.tsx
@@ -1,20 +1,12 @@
 "use client";
 import FramedCard from "@/components/FramedCardOrSkill";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/Tooltip";
 import { genCardsAndEncounters } from "@/lib/Data";
 
-const { Cards: CardsData, Encounters: EncounterData } =
-  await genCardsAndEncounters();
+const { Cards: CardsData } = await genCardsAndEncounters();
 
 export default function FramePage() {
   return (
     <>
-      <Tooltip>
-        <TooltipTrigger>
-          <button className="px-2 py-1">❔</button>
-        </TooltipTrigger>
-        <TooltipContent>Tooltip content, hello world!</TooltipContent>
-      </Tooltip>
       <div className="text-foreground max-h-full gap-4 overflow-hidden p-4">
         {CardsData["0.1.9"].slice(0, 10).map((card) => (
           <FramedCard key={card.Id} card={card} />
