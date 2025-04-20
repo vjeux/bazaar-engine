f4af3d7c10498b3506d035414828ce386e1436f2
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-04-20 12:57:18 +0200

    refactor: integrate FramedCardOrSkill component in BoardSkillElement

--- src/components/BoardSkillElement.tsx
+++ src/components/BoardSkillElement.tsx
@@ -2,6 +2,7 @@
 import type { BoardSkill, GameState } from "@/engine/Engine";
 import React from "react";
 import { Tooltip } from "../../app/old/-App";
+import FramedCardOrSkill from "./FramedCardOrSkill";
 
 export function BoardSkillElement({
   boardSkill,
@@ -15,18 +16,7 @@ export function BoardSkillElement({
   boardCardID: number;
 }) {
   const card = boardSkill.card;
-  const tier = boardSkill.tier;
-
   const IMAGE_SIZE = 60;
-  const borderSize = 0.1;
-
-  const localeText = card.Localization.Title.Text;
-  const imgUrl = `https://www.howbazaar.gg/images/skills/${localeText.replace(
-    /[ '\-&]/g,
-    "",
-  )}.avif`;
-
-  const frameUrl = `https://www.bazaarplanner.com/images/fromBT/skill_tier_${tier.toLowerCase()}.png`;
 
   return (
     <div
@@ -43,20 +33,11 @@ export function BoardSkillElement({
           width: IMAGE_SIZE,
         }}
       >
-        {/* Image container */}
-        <div
-          className="absolute rounded-full"
-          style={{
-            top: IMAGE_SIZE * borderSize,
-            left: IMAGE_SIZE * borderSize,
-            right: IMAGE_SIZE * borderSize,
-            bottom: IMAGE_SIZE * borderSize,
-          }}
-        >
-          <img src={imgUrl} className="h-full w-full rounded-full" />
-        </div>
-        {/* Frame image */}
-        <img src={frameUrl} className="absolute top-0 left-0 h-full w-full" />
+        <FramedCardOrSkill
+          card={card}
+          skillSize={IMAGE_SIZE}
+          tier={boardSkill.tier}
+        />
       </div>
       <Tooltip
         boardCard={boardSkill}
