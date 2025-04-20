8a4bb52d04321ca82ed0453d0af73fcc6e2d9216
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-04-20 12:45:46 +0200

    feat: common cardItem/skill framed image component

--- app/frame/page.tsx
+++ app/frame/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 import FramedCard from "@/components/FramedCardOrSkill";
 import { genCardsAndEncounters } from "@/lib/Data";
+import { Card } from "@/types/cardTypes";
+import { CardType } from "@/types/cardTypes";
 
 const { Cards: CardsData } = await genCardsAndEncounters();
 
 export default function FramePage() {
+  const filteredCards = CardsData["0.1.9"].filter(
+    (item: Card) => item.$type === CardType.TCardItem,
+  );
+  const filteredSkills = CardsData["0.1.9"].filter(
+    (item) => item.$type === CardType.TCardSkill,
+  );
   return (
     <>
       <div className="text-foreground max-h-full gap-4 overflow-hidden p-4">
-        {CardsData["0.1.9"].slice(0, 10).map((card) => (
+        {filteredCards.slice(0, 10).map((card) => (
           <FramedCard key={card.Id} card={card} />
+        ))}
+        {filteredSkills.slice(0, 10).map((skill) => (
+          <FramedCard key={skill.Id} card={skill} />
         ))}
       </div>
     </>
--- src/components/FramedCardOrSkill.tsx
+++ src/components/FramedCardOrSkill.tsx
@@ -1,21 +1,81 @@
 import { Card, CardType } from "@/types/cardTypes";
 
-export default function FramedCard({ card }: { card: Card }) {
+interface FramedCardOrSkillProps {
+  card: Card;
+  cardHeight?: number;
+  skillSize?: number;
+}
+
+export default function FramedCardOrSkill({
+  card,
+  cardHeight = 100,
+  skillSize = 60,
+}: FramedCardOrSkillProps) {
   const isCardItem = card.$type === CardType.TCardItem;
 
   const imageUrl = `https://www.howbazaar.gg/images/${isCardItem ? "items" : "skills"}/${
     card.Localization.Title.Text?.replace(/[ '\-&]/g, "") ?? ""
   }.avif`;
+
   const frameUrl = isCardItem
     ? `https://www.bazaarplanner.com/images/fromBT/CardFrame_${card.StartingTier}_${
         { Large: "L", Medium: "M", Small: "S" }[card.Size]
       }_TUI.png`
     : `https://www.bazaarplanner.com/images/fromBT/skill_tier_${card.StartingTier.toLowerCase()}.png`;
 
+  const cardWidth = isCardItem
+    ? card.Size === "Small"
+      ? cardHeight / 2
+      : card.Size === "Medium"
+        ? cardHeight
+        : cardHeight * 1.5
+    : skillSize;
+
+  const padding = isCardItem
+    ? {
+        top: 0.06,
+        left: 0.03,
+        bottom: 0.1,
+        right: 0.04,
+      }
+    : {
+        top: 0.1,
+        left: 0.1,
+        bottom: 0.1,
+        right: 0.1,
+      };
+
   return (
-    <div>
-      <img src={imageUrl} />
-      <img src={frameUrl} />
+    <div
+      className="relative"
+      style={{
+        height: isCardItem ? cardHeight : skillSize,
+        width: cardWidth,
+      }}
+    >
+      {/* Image container */}
+      <div
+        className="absolute"
+        style={{
+          top: isCardItem ? cardHeight * padding.top : skillSize * padding.top,
+          left: isCardItem
+            ? cardHeight * padding.left
+            : skillSize * padding.left,
+          bottom: isCardItem
+            ? cardHeight * padding.bottom
+            : skillSize * padding.bottom,
+          right: isCardItem
+            ? cardHeight * padding.right
+            : skillSize * padding.right,
+        }}
+      >
+        <img
+          src={imageUrl}
+          className={`h-full w-full ${isCardItem ? "rounded-[5px]" : "rounded-full"}`}
+        />
+      </div>
+      {/* Frame image */}
+      <img src={frameUrl} className="absolute h-full w-full" />
     </div>
   );
 }
