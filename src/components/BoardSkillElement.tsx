f75714d8e7abc5e83e4a1eca1b81808ff7ef7454
Magnus Strømseng <magnus.stromseng@gmail.com>
2025-05-11 17:57:39 +0200

    feat: skill removal

--- src/components/BoardSkillElement.tsx
+++ src/components/BoardSkillElement.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import FramedCardOrSkill from "./FramedCardOrSkill";
 import TooltipWithGameState from "./TooltipWithGameState";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./Tooltip";
+import { useSimulatorStore } from "@/lib/simulatorStore";
+import { CardType } from "@/types/cardTypes";
 
 export function BoardSkillElement({
   boardSkill,
@@ -16,6 +18,8 @@ export function BoardSkillElement({
   playerID: number;
   boardCardID: number;
 }) {
+  const simulatorStoreActions = useSimulatorStore((state) => state.actions);
+
   const card = boardSkill.card;
   const IMAGE_SIZE = 60;
 
@@ -23,7 +27,7 @@ export function BoardSkillElement({
     <Tooltip placement="bottom">
       <TooltipTrigger>
         <div
-          className="tooltipContainer"
+          className="tooltipContainer relative"
           onClick={() => {
             console.log(boardSkill);
           }}
@@ -33,6 +37,23 @@ export function BoardSkillElement({
             skillSize={IMAGE_SIZE}
             tier={boardSkill.tier}
           />
+          {/* Remove button */}
+          <button
+            type="button"
+            className="tooltip absolute top-0.5 left-0.5 z-50 hover:cursor-pointer"
+            onClick={() => {
+              const actualIndex = gameState.players[playerID].board.findIndex(
+                (x) => x.uuid === boardSkill.uuid,
+              );
+              const lastCardIndex = gameState.players[
+                playerID
+              ].board.findLastIndex((x) => x.card.$type === CardType.TCardItem);
+              const skillIndex = actualIndex - lastCardIndex - 1;
+              simulatorStoreActions.removePlayerSkill(skillIndex);
+            }}
+          >
+            ❌
+          </button>
         </div>
       </TooltipTrigger>
       <TooltipContent>
